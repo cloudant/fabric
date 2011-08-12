c62787bb4387c23442509c465622ab0b3a43c1f8
Adam Kocoloski <adam@cloudant.com>
2011-08-11 23:41:28 -0400

    Restore kv/2 export

--- src/fabric_util.erl
+++ src/fabric_util.erl
@@ -15,7 +15,8 @@
 -module(fabric_util).
 
 -export([submit_jobs/3, cleanup/1, recv/4, get_db/1, get_db/2, error_info/1,
-        update_counter/3, remove_ancestors/2, create_monitors/1, quorum_met/2]).
+        update_counter/3, remove_ancestors/2, create_monitors/1, quorum_met/2,
+        kv/2]).
 
 -include("fabric.hrl").
 -include_lib("mem3/include/mem3.hrl").
