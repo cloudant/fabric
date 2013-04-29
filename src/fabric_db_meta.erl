c2924c94ff3b6d37609a8f4a51c1e6a607647dfe
Adam Kocoloski <adam@cloudant.com>
2013-04-29 13:44:00 -0400

    Enhance maintenance_mode instead of creating a new mode
    BugzID: 18955

--- src/fabric_db_meta.erl
+++ src/fabric_db_meta.erl
@@ -78,7 +78,7 @@ handle_set_message(ok, W, Acc) ->
         finished = [W | Acc#acc.finished]
     },
     maybe_finish_set(NewAcc);
-handle_set_message({rexi_EXIT, {non_interactive_mode, _}}, W, Acc) ->
+handle_set_message({rexi_EXIT, {maintenance_mode, _}}, W, Acc) ->
     NewAcc = Acc#acc{workers = (Acc#acc.workers -- [W])},
     maybe_finish_set(NewAcc);
 handle_set_message(Error, W, Acc) ->
@@ -150,7 +150,7 @@ handle_get_message({Props}=SecObj, W, Acc) when is_list(Props) ->
         finished = [{W, SecObj} | Acc#acc.finished]
     },
     maybe_finish_get(NewAcc);
-handle_get_message({rexi_EXIT, {non_interactive_mode, _}}, W, Acc) ->
+handle_get_message({rexi_EXIT, {maintenance_mode, _}}, W, Acc) ->
     NewAcc = Acc#acc{workers = (Acc#acc.workers -- [W])},
     maybe_finish_get(NewAcc);
 handle_get_message(Error, W, Acc) ->
