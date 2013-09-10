dcbf7f0fe9bfeaa0bb524f3e19cd0a504d3c4985
Robert Newson <robert.newson@cloudant.com>
2013-09-10 13:59:53 +0100

    Handle new causes of not_found

--- src/fabric_db_doc_count.erl
+++ src/fabric_db_doc_count.erl
@@ -52,6 +52,9 @@ handle_message({rexi_EXIT, Reason}, Shard, {Counters, Acc}) ->
         {error, Reason}
     end;
 
+handle_message({not_found, Reason}, _, _) ->
+    throw({not_found, Reason});
+
 handle_message({ok, Count}, Shard, {Counters, Acc}) ->
     case fabric_dict:lookup_element(Shard, Counters) of
     undefined ->
