4af69e0a65764f9637c189c2f09d341605ab8d00
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-10-28 16:43:18 -0500

    Revert "Update changes workers with new rexi APIs"
    This reverts commit abe37938333a59abaee17720450a2923c6ce0482.

--- src/fabric_rpc2.erl
+++ src/fabric_rpc2.erl
@@ -93,13 +93,10 @@ changes(DbName, Options, StartVector, DbOptions) ->
         StartSeq = calculate_start_seq(Db, node(), StartVector),
         Enum = fun changes_enumerator/2,
         Opts = [{dir,Dir}],
-        Acc0 = {Db, StartSeq, Args, Options, false},
+        Acc0 = {Db, StartSeq, Args, Options},
         try
-            {ok, {_, LastSeq, _, _, Started}} =
+            {ok, {_, LastSeq, _, _}} =
                 couch_db:changes_since(Db, StartSeq, Enum, Opts, Acc0),
-            if Started == true -> ok; true ->
-                rexi:stream_init()
-            end,
             rexi:reply({complete, {LastSeq, uuid(Db)}})
         after
             couch_db:close(Db)
@@ -453,13 +450,10 @@ send(Key, Value, #view_acc{limit=Limit} = Acc) ->
     rexi:stream(#view_row{key=Key, value=Value}),
     {ok, Acc#view_acc{limit=Limit-1}}.
 
-changes_enumerator(DocInfo, {Db, Seq, Args, Options, false}) ->
-    rexi:stream_init(),
-    changes_enumerator(DocInfo, {Db, Seq, Args, Options, true});
 changes_enumerator(#doc_info{id= <<"_local/", _/binary>>, high_seq=Seq},
-        {Db, _OldSeq, Args, Options, Started}) ->
-    {ok, {Db, Seq, Args, Options, Started}};
-changes_enumerator(DocInfo, {Db, _Seq, Args, Options, Started}) ->
+        {Db, _OldSeq, Args, Options}) ->
+    {ok, {Db, Seq, Args, Options}};
+changes_enumerator(DocInfo, {Db, _Seq, Args, Options}) ->
     #changes_args{
         include_docs = IncludeDocs,
         filter = Acc
@@ -468,12 +462,12 @@ changes_enumerator(DocInfo, {Db, _Seq, Args, Options, Started}) ->
     #doc_info{high_seq=Seq, revs=[#rev_info{deleted=Del}|_]} = DocInfo,
     case [X || X <- couch_changes:filter(DocInfo, Acc), X /= null] of
     [] ->
-        {ok, {Db, Seq, Args, Options, Started}};
+        {ok, {Db, Seq, Args, Options}};
     Results ->
         Opts = if Conflicts -> [conflicts]; true -> [] end,
         ChangesRow = changes_row(Db, DocInfo, Results, Del, IncludeDocs, Opts),
-        rexi:stream(ChangesRow),
-        {ok, {Db, Seq, Args, Options, Started}}
+        Go = rexi:sync_reply(ChangesRow),
+        {Go, {Db, Seq, Args, Options}}
     end.
 
 changes_row(Db, #doc_info{id=Id, high_seq=Seq}=DI, Results, Del, true, Opts) ->
